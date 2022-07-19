44858b82b85dffef05cefe6faffba072084bdc24
Akuro <shocked1370@gmail.com>
2022-07-18 23:06:02 -0500

    Coming soon v1.0.0

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BASSE | Anyone can be an artist.</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body theme="night">
+        <div class="navbar">
+            <div class="navcontent">
+                <img src="assets/images/branding/navicon.png">
+            </div>
+        </div>
+        <div class="content">
+            <div class="row">
+                <div class="column">
+                    <div style="padding: 50px;">
+                        <h1>Coming <span class="text-primary">s<span class="nolig">oo</span>n.</span></h1><br><br>
+                        <p style="width: 600px;">BASSE <span class="text-primary">will be</span> a place for new music artists to share their music and for existing artists to grow their fanbase.</p><br><br>
+                        <button class="side" disabled style="cursor: default;">Learn More</button>
+                        <button disabled>Meet the Developers</button><br><br>
+                        <p style="opacity: 0.2;">Coming soon!</p>
+                    </div>
+                </div>
+                <div class="column">
+                    <img src="assets/images/hero.svg">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
